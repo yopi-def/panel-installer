edf15afc13b82fac75c0af0789ef3f59de539e42
yopi-def <yopiahmadafrizal08@gmail.com>
2025-12-11 09:12:09 +0700

    Initial commit from Termux

---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -e
+
+export GITHUB_SOURCE="v1.2.0"
+export SCRIPT_RELEASE="v1.2.0"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/yopi-def/panel-installer"
+
+LOG_PATH="/var/log/pterodactyl-installer.log"
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+# Always remove lib.sh, before downloading it
+[ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
+curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/main/lib/lib.sh
+# shellcheck source=lib/lib.sh
+source /tmp/lib.sh
+
+execute() {
+  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+
+  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="main" && export SCRIPT_RELEASE="canary"
+  update_lib_source
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
+
+  if [[ -n $2 ]]; then
+    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    read -r CONFIRM
+    if [[ "$CONFIRM" =~ [Yy] ]]; then
+      execute "$2"
+    else
+      error "Installation of $2 aborted."
+      exit 1
+    fi
+  fi
+}
+
+welcome ""
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install Panel"
+    "Install Wings"
+    "Uninstall Panel"
+  )
+
+  actions=(
+    "panel"
+    "wings"
+    "uninstall_canary"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
+
+# Remove lib.sh, so next time the script is run the, newest version is downloaded.
+rm -rf /tmp/lib.sh
